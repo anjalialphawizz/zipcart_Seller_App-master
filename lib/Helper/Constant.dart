70506ebb29538afa453b8da92b1e5f274fac085f
anjalialphawizz <anjalialphawizz>
2024-02-05 17:44:30 +0530

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,22 @@
+final String appName = 'Zipcart Restaurant';
+final String packageName = 'com.zipcart.sellerapp';
+
+final String iosPackage = 'com.zipcart.sellerapp';
+
+// https://alphawizztest.tk/Food_on_the_way/app/v1/api/
+// final String baseUrl = 'https://foodontheways.com/seller/app/v1/api/';
+//final String baseUrl = 'https://foodontheways.com/New_food/seller/app/v1/api/';
+// /*final String baseUrl = 'https://eatoz.in/seller/app/v1/api/';*/
+//final String baseUrl = 'https://zipcart.co.in/seller/app/v1/api/';
+final String baseUrl = 'https://zipcart.co.in/seller/app/v1/api/';
+// final String baseUrl = 'https://developmentalphawizz.com/eatoz/seller/app/v1/api/';
+final String jwtKey = "f78d95e500c7ef84ee99d69d9651ff7a229979a2";
+
+//timeout limit
+final int timeOut = 50;
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
